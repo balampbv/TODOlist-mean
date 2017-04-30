6c39a136f903f45ae7ea1551216c7a744677c25e
balampbv <balampbv007@gmail.com>
2017-04-30 13:19:47 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var tasks = require('./routes/tasks');
+
+var app = express();
+
+//View Engine
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+
+
+//set Static Folder
+app.use(express.static(path.join(__dirname,'client')));
+
+//Body parser MW
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+
+app.use('/',index);
+app.use('/api',tasks);
+
+
+
+app.listen(3003,function(){
+	console.log("Server is running!!!");
+})
